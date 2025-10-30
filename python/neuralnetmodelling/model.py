27bb5e852020c2f02f768447052725a687862063
Gerald Mwangi <geraldmwangi@gmx.de>
2025-10-30 18:21:53 +0100

    checked out 2d model from commit 10909d2206ba7a356ebead564f036a29a8fb71c5

--- python/neuralnetmodelling/model.py
+++ python/neuralnetmodelling/model.py
@@ -1,89 +1,30 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
-
-def conv_block(x,numfilters):
-    x = layers.Conv1D(numfilters, 3, padding='same', use_bias=False)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation('relu')(x)
-    return x
-
 def build_cnn_model(input_shape, output_dim,training=True):
-    return build_small_cnn_model(input_shape, output_dim,training=True)
+    model = models.Sequential()
+    model.add(layers.Input(shape=input_shape, dtype=tf.float32))
 
+    model.add(layers.Conv2D(filters=32, kernel_size=(3, 3), padding='same', activation='relu'))
+    model.add(layers.BatchNormalization())
+    if training:
+        model.add(layers.SpatialDropout2D(0.2))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-def build_small_cnn_model(input_shape, output_dim,training=True):
-    print("Building CNN model with input shape:", input_shape, "and output dim:", output_dim)
-    inputs = layers.Input(shape=input_shape)
-    
-    # Conv block 1
-    x=conv_block(inputs,32)
-    x = layers.MaxPooling1D(pool_size=4, strides=4)(x)
-    x = layers.SpatialDropout1D(0.2)(x, training=training)
-    x=conv_block(x,64)
+    model.add(layers.Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
+    model.add(layers.BatchNormalization())
+    if training:
+        model.add(layers.SpatialDropout2D(0.2))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-    
-    x = layers.MaxPooling1D(pool_size=2, strides=2)(x)
-    x = layers.SpatialDropout1D(0.2)(x, training=training)
-    # x = layers.Dropout(0.2)(x, training=training)
-    x=conv_block(x,64)
+    model.add(layers.Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu'))
+    if training:
+        model.add(layers.BatchNormalization())
+    model.add(layers.SpatialDropout2D(0.3))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-    
-    
-    x = layers.MaxPooling1D(pool_size=2, strides=2)(x)
-    x = layers.SpatialDropout1D(0.3)(x, training=training)
-    # x = layers.Dropout(0.2)(x, training=training)
+    model.add(layers.GlobalAveragePooling2D())
+    if training:
+        model.add(layers.Dropout(0.2))
+    model.add(layers.Dense(output_dim, activation='sigmoid', dtype=tf.float32))
 
-    x = layers.GlobalAveragePooling1D()(x)
-    # x=layers.Flatten()(x)
-    # x = layers.GlobalMaxPooling1D()(x)
-    # x=layers.Dense(128,activation='linear')(x)
-    x = layers.Dropout(0.2)(x, training=training)
-    outputs = layers.Dense(output_dim, activation='sigmoid')(x)
-    
-    model = models.Model(inputs, outputs)
     return model
-
-
-    # model = models.Sequential()
-    # model.add(layers.Input(shape=input_shape, dtype=tf.float32))
-    # model.add(layers.Dense(250, activation='relu', dtype=tf.float32))
-    # model.add(layers.Dense(150, activation='relu', dtype=tf.float32))
-    # model.add(layers.GlobalAveragePooling1D())
-    # model.add(layers.Dense(output_dim, activation='sigmoid', dtype=tf.float32))
-    return model
-
-def build_cnn_autoencoder(input_shape, output_dim, training=True):
-    inputs = layers.Input(shape=input_shape, dtype=tf.float32)
-    
-    # Encoder
-    x = conv_block(inputs, 32)
-    x = conv_block(x, 64)
-    x = layers.MaxPooling1D(pool_size=2, strides=2)(x)
-    x = layers.SpatialDropout1D(0.2)(x, training=training)
-    x = conv_block(x, 128)
-    x = layers.MaxPooling1D(pool_size=2, strides=2)(x)
-    x = layers.SpatialDropout1D(0.2)(x, training=training)
-
-    # Latent representation with 6 neurons
-    x = layers.GlobalMaxPooling1D()(x)
-    latent = layers.Dense(6, activation='linear', name='latent_vector')(x)
-    
-    # Decoder
-    # Upsample from latent space
-    upsample_length = output_dim  # accounting for 2 pooling layers (each halves length)
-    x = layers.Dense(upsample_length * 128, activation='relu')(latent)
-    x = layers.Reshape((upsample_length, 128))(x)
-    
-    x = layers.UpSampling1D(size=2)(x)
-    x = conv_block(x, 64)
-    x = layers.UpSampling1D(size=2)(x)
-    x = conv_block(x, 32)
-    x = layers.MaxPooling1D(pool_size=2, strides=2)(x)
-    x = layers.SpatialDropout1D(0.3)(x, training=training)
-    
-    x = layers.GlobalMaxPooling1D()(x)
-    x=layers.Dense(128,activation='linear')(x)
-    x = layers.Dropout(0.2)(x, training=training)
-    outputs = layers.Dense(output_dim, activation='sigmoid', dtype=tf.float32)(x)
-    autoencoder = models.Model(inputs, outputs, name="autoencoder")
-    return autoencoder
