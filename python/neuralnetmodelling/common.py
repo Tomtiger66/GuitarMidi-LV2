96e44dde78bf2b66a51838293df99cea9b25f9ab
Gerald Mwangi <geraldmwangi@gmx.de>
2026-01-11 18:21:37 +0100

    finally got its working

--- python/neuralnetmodelling/common.py
+++ python/neuralnetmodelling/common.py
@@ -52,39 +52,76 @@ def load_sample_from_files(input_path_tensor):
     # print("output: "+output_path)
     # Load data
 
-    image = np.load(input_path).astype(np.float32)#.reshape(INPUT_SHAPE)
+    images = np.load(input_path).astype(np.float32)#.reshape(INPUT_SHAPE)
     
-    print("Image shape: ",image.shape)
-    label = np.load(output_path).astype(np.float32)#.reshape(OUTPUT_DIM_NOTES)
+    print("Image shape: ",images.shape)
+    labels = np.load(output_path).astype(np.float32)#.reshape(OUTPUT_DIM_NOTES)
     
-    print("Label shape: ",label.shape)
+    print("Label shape: ",labels.shape)
 
+    image=[]
+    label=[]
+    # Reshape data
+    for i in range(10):#images.shape[0]):
+        img=images[i].reshape(INPUT_SHAPE)
+        lbl=labels[i].reshape((OUTPUT_DIM_NOTES,))
+        #Ensure shape
+        img=tf.ensure_shape(img, INPUT_SHAPE)
+        lbl=tf.ensure_shape(lbl, (OUTPUT_DIM_NOTES,)) 
+        # Append to list
+        image.append(img)
+        label.append(lbl)
     # Ensure shape
     # image = tf.ensure_shape(image, INPUT_SHAPE)
     # label = tf.ensure_shape(label, (OUTPUT_DIM_NOTES,)) 
     
     # Return features and label
-    return image, label
+    #print sizes of image and label
+    print("Loaded sample from ",input_path)
+    print("Image shape: ",len(image),image[0].shape)
+    print("Label shape: ",len(label),label[0].shape)
+    return image,label  # Return (features, labels)
 
 
 
-def load_chunk_from_file(ipath_tensor, opath_tensor):
-    ipath = ipath_tensor.numpy().decode('utf-8')
-    opath = opath_tensor.numpy().decode('utf-8')
+def load_chunk_from_file(input_path_tensor):
+    input_path = input_path_tensor.numpy().decode('utf-8')
+    #print("Loaded sample from ",input_path)
+    inputname=os.path.basename(input_path)
     
-    # Use mmap_mode='r' to read only what is needed from the SATA HDD
-    image_chunk = np.load(ipath, mmap_mode='r').astype(np.float32)
-    label_chunk = np.load(opath, mmap_mode='r').astype(np.float32)
+    parentdir=os.path.dirname(os.path.dirname(input_path))
+    # print("current dir: "+parentdir)
+    output_path=os.path.join(parentdir,'output',inputname)
+
+    # print("input: "+input_path)
+    # print("output: "+output_path)
+    # Load data
+
+    images = np.load(input_path, mmap_mode='r').astype(np.float32)#.reshape(INPUT_SHAPE)
+    
+    #print("Image shape: ",images.shape)
+    labels = np.load(output_path, mmap_mode='r').astype(np.float32)#.reshape(OUTPUT_DIM_NOTES)
+    
+    #print("Label shape: ",labels.shape)
+    
+    image_chunk=images.reshape((SAMPLES_PER_CHUNK,image_height,image_width,num_channels))
+    label_chunk=labels.reshape((SAMPLES_PER_CHUNK,OUTPUT_DIM_NOTES))
     
     return image_chunk, label_chunk
 
-def tf_load_chunk(ipath, opath):
+def tf_load_chunk(ipath):
     # This returns the full 316-sample blocks
-    return tf.py_function(load_chunk_from_file, [ipath, opath], [tf.float32, tf.float32])
+    return tf.py_function(load_chunk_from_file, [ipath], [tf.float32, tf.float32])
 
-def unchunk_mapper(ipath, opath):
-    img_chunk, lbl_chunk = tf_load_chunk(ipath, opath)
-    # This turns the (316, ...) array into a stream of 316 individual samples
+def unchunk_mapper(ipath):
+    # img_chunk shape: (316, 312, 256, 1)
+    # lbl_chunk shape: (316, OUTPUT_DIM_NOTES)
+    img_chunk, lbl_chunk = tf_load_chunk(ipath)
+    
+# Explicitly set the shapes of the chunks before slicing
+    img_chunk.set_shape([None, 312, 256, 1])
+    lbl_chunk.set_shape([None, OUTPUT_DIM_NOTES])
+    # from_tensor_slices slices along the FIRST dimension (the 316)
     return tf.data.Dataset.from_tensor_slices((img_chunk, lbl_chunk))
 
 # TensorFlow wrapper for loading sample from files
@@ -92,15 +129,11 @@ def tf_load_sample_from_files(ipath):
     image, label = tf.py_function(
         load_sample_from_files, [ipath], [tf.float32, tf.float32]
     )
-    print("Loaded sample from ",ipath)
-    print("Image shape: ",image.shape)
-    print("Label shape: ",label.shape)
-    image.set_shape([None, INPUT_SHAPE[0], INPUT_SHAPE[1]])#,INPUT_SHAPE[2]])#image.set_shape(INPUT_SHAPE)
-    label.set_shape([None, OUTPUT_DIM_NOTES])#label.set_shape((OUTPUT_DIM_NOTES,))
-    print("Loaded sample from ",ipath)
-    print("Image shape: ",image.shape)
-    print("Label shape: ",label.shape)
-    return image,label#tf.data.Dataset.from_tensor_slices((image, label)) # Return (features, labels, sample_weights)   
+
+    image.set_shape(INPUT_SHAPE)
+    label.set_shape((OUTPUT_DIM_NOTES,))
+
+    return image,label,#tf.data.Dataset.from_tensor_slices((image, label)) # Return (features, labels, sample_weights)   
     
 def plot_heatmap(plotdata,downsample_factor=1000):
     num_cols=plotdata.shape[1]
