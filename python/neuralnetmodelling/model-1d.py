f6b3dec67cb523d9b34acebe6bbaa145c5c8eb12
Gerald Mwangi <geraldmwangi@gmx.de>
2026-02-12 17:21:23 +0100

    updated old model

--- python/neuralnetmodelling/model-1d.py
+++ python/neuralnetmodelling/model-1d.py
@@ -1,22 +1,39 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
+from tensorflow.keras.models import Model
+from common import OUTPUT_DIM_NOTES,image_height,image_width
 
+    # --- Configuration for your specific input ---
+IMG_H, IMG_W = 312, 256  
+NUM_CLASSES = 89        
+PATCH_H, PATCH_W = 24, 16 # Chosen patch size
+CHANNELS = 1            
+HIDDEN_D = 384           # Chosen hidden dimension
+TRANSFORMER_LAYERS = 4   # Chosen layer count
+NUM_HEADS = 6            # Chosen number of heads (divisor of 384)
+DROPOUT_RATE = 0.1
 def conv_block(x,numfilters):
-    x = layers.Conv1D(numfilters, 3, padding='same', use_bias=False)(x)
+    x = layers.Conv1D(numfilters, 3, padding='same', activation=None)(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation('relu')(x)
     return x
+def partitioned_average_pooling(x):
+    splits = tf.split(x, [7,7,7,6,6,6], axis=1)   # sum=39 height slices
+    pooled = [tf.reduce_mean(part, axis=[1,2]) for part in splits]
+    return tf.concat(pooled, axis=1)
 
-def build_cnn_model(input_shape, output_dim,training=True):
-    return build_small_cnn_model(input_shape, output_dim,training=True)
+def partitioned_average_pooling_1d(x):
+    splits = tf.split(x, [7,7,7,6,6,6], axis=1)   # sum=39 height slices
+    pooled = [tf.reduce_mean(part, axis=[1]) for part in splits]
+    return tf.concat(pooled, axis=1)
 
-
-def build_small_cnn_model(input_shape, output_dim,training=True):
+def build_1d_cnn_model(batch_sz=64, input_shape=(image_height, image_width), output_dim=OUTPUT_DIM_NOTES, training=True,
+                       gru_units=128, gru_layers=1, bidirectional=True, stateful=False):  # Added GRU params
     print("Building CNN model with input shape:", input_shape, "and output dim:", output_dim)
     inputs = layers.Input(shape=input_shape)
-    
+    x=layers.Lambda(lambda x: tf.reduce_max(x, axis=2))(inputs)
     # Conv block 1
-    x=conv_block(inputs,32)
+    x=conv_block(x,32)
     x = layers.MaxPooling1D(pool_size=4, strides=4)(x)
     x = layers.SpatialDropout1D(0.2)(x, training=training)
     x=conv_block(x,64)
@@ -44,13 +61,7 @@ def build_small_cnn_model(input_shape, output_dim,training=True):
     return model
 
 
-    # model = models.Sequential()
-    # model.add(layers.Input(shape=input_shape, dtype=tf.float32))
-    # model.add(layers.Dense(250, activation='relu', dtype=tf.float32))
-    # model.add(layers.Dense(150, activation='relu', dtype=tf.float32))
-    # model.add(layers.GlobalAveragePooling1D())
-    # model.add(layers.Dense(output_dim, activation='sigmoid', dtype=tf.float32))
-    return model
+
 
 def build_cnn_autoencoder(input_shape, output_dim, training=True):
     inputs = layers.Input(shape=input_shape, dtype=tf.float32)
